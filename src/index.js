fbb7e6a2e8f4e1fe8a2a7d57137965a0f563faf5
claudiacortezb <claudia.cboc@gmail.com>
2020-02-18 09:47:31 -0500

    avnce jeje

--- src/index.js
+++ src/index.js
@@ -1,3 +1,63 @@
 import validator from './validator.js';
 
+const screen1Elem=document.getElementById('screen1');
+const screen2Elem=document.getElementById('screen2');
+const screenValidElem=document.getElementById('screenValid');
+const screenInvalidElem=document.getElementById('screenInvalid');
+
+
+//First
+const inputElem =document.getElementById("inputName");//tiene value
+//Second
+const spanNameElem=document.getElementsByName('spanName')[0]; //tiene value
+const numTarElem=document.getElementById('numTar'); //tiene value
+    //const creditCardNumber =numTarValue; //por el momento inexistente
+
+//Third
+const spanNumberElem=document.getElementsByName('spanNumber')[0];
+
+//buttons
+const welcomButElem=document.getElementById('welcomBut');
+const validButElem=document.getElementById('validBut');
+const againBut1Elem=document.getElementsByClassName('againBut')[0];
+const againBut2Elem=document.getElementsByClassName('againBut')[1];
+const outBut1Elem=document.getElementsByClassName('outBut')[0];
+const outBut2Elem=document.getElementsByClassName('outBut')[1];
+
+//no se si debe ir aqui
+let userName;
+const sendName=()=>{
+    const inputValue=inputElem.value;//debe estar dentro del sendName,sino no toma el value
+    if(inputValue.trim()==('') ){
+        alert('Por favor ingrese su nombre');
+    }
+    else if (isNaN(inputValue)===false) {
+        alert('Por favor ingrese un nombre correcto')
+    }
+    else {
+        userName=inputValue.trim();
+        screen1Elem.style.display='none';
+        spanNameElem.textContent=userName;
+        screen2Elem.style.display='block';
+        console.log(userName);       
+    }    
+}
+welcomButElem.addEventListener('click',sendName);
+
+validButElem.addEventListener('click',isValid);
+againBut1Elem.addEventListener('click',again);
+againBut2Elem.addEventListener('click',again);
+
+
+
+
+
+
+
+
+
+
+
+
+    
 console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,48 @@
 const validator = {
-  // ...
+  isValid: () => {    
+    const numTarValue=numTarElem.value;
+    //const isValid = creditCardNumber => {  //por el momento inexistente
+    if(isNaN(numTarValue)) {
+        alert('Ingrese solo numeros')   
+    }
+    else{
+        //let arr = (creditCardNumber  +'') // por el momento inexistente
+        let  arr = (numTarValue +'') 
+        .split('')
+        .reverse()
+        .map(str=>parseInt(str))
+        //revierte y convierte a num el array
+        console.log(arr);
+        let newArr=[];
+        let sum=0;
+        for(i=0;i<arr.length;i++){ 
+            newArr[i]=(i%2==0)? arr[i]*2  :arr[i]; //agrego los elementos al array sin push al usar newArr[i] (if index es par lo duplico,sino lo dejo normal) 
+            console.log(newArr);
+            //if (newArr[i]>9){ newArr[i] -=9} mejor uso el Op. ternario
+            newArr[i]>9 ? newArr[i]-=9 :newArr[i];
+            sum+=newArr[i];
+        } 
+        let tenMulti=sum%10===0;
+        showResult(tenMulti);
+        return tenMulti; 
+    }    
+  };
+  ,// separo los metodos 
+  
+  maskify:(num)=>{
+    arrRev=(num+'').split('').reverse()
+      .map(str=>parseInt(str));
+    
+    let newArr=[];
+    for(i=0;i<arrRev.length;i++){
+      newArr[i]= i>=4? newArr[i]='#': arrRev[i];
+    }
+    arrFinal=newArr.reverse().join(''); //piden que sea string y sin separacion->join
+    console.log(arrFinal);
+    return arrFinal;
+  }
 };
+
+
 
 export default validator;
